b020ba2e9d7268fde65cea52c3284e0b25bbc143
ZSRe <103521044+devredakun@users.noreply.github.com>
2026-01-20 11:54:33 +0100

    Add files via upload

---
+++ hardware/lichtsensor.py
@@ -0,0 +1,15 @@
+from gpiozero import DigitalInputDevice
+from time import sleep
+
+lichtsensor = DigitalInputDevice(6)  # BCM 5
+
+try:
+    while True:
+        if lichtsensor.value:
+            print("Es ist dunkel")
+        else:
+            print("Es werde Licht")
+        sleep(1)
+
+except KeyboardInterrupt:
+    print("Programm beendet")
